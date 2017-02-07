bf987dcd9673129c3f953bb52c6bba35b7f9f81b
pcchen <cpc_geek@163.com>
2017-02-07 15:47:43 +0800

    flume用户自定义拦截器---定义

---
+++ src/org/pcchen/interceptor/UserDefineInterceptor.java
@@ -0,0 +1,12 @@
+package org.pcchen.interceptor;
+/**
+ * File: UserDefineInterceptor.java 
+ * Author:pcchen
+ * Email：cpc_geek@163.com
+ * Date: 2017年2月7日下午3:46:03
+ * Description: 
+ */
+
+public class UserDefineInterceptor {
+	
+}
